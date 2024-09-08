dc1b4cb2a2f9b61559b5390f94629dd2303772ed
Karan Pashte <111000773+karangh1@users.noreply.github.com>
2024-09-08 12:06:30 +0530

    Add files via upload

---
+++ constant/data.js
@@ -0,0 +1,58 @@
+const data=[
+    {
+        "name": "Acne and Rosacea Photos",
+        "symptoms": `1. Acne often presents as pimples, blackheads, and whiteheads on the face, chest, and back.
+
+2. Rosacea is characterized by facial redness, visible blood vessels, and sometimes small, red, pus-filled bumps.`,
+        "remedies": "Treatment for acne may include topical creams, oral medications, or procedures like chemical peels or laser therapy. Rosacea treatment often involves topical or oral antibiotics, topical creams, and lifestyle changes to avoid triggers.",
+        "img":"Actinic" 
+      },
+    {
+        "name": "Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions",
+        "symptoms": `1. Actinic keratosis presents as rough, scaly patches on sun-exposed areas of the skin. 
+
+2. Basal cell carcinoma may appear as shiny, pearly bumps or as pink, translucent growths with rolled edges`,
+        "remedies": `1, Treatment for actinic keratosis may include cryotherapy, topical medications, or procedures like photodynamic therapy.
+        
+2. Basal cell carcinoma treatment may involve surgery, radiation therapy, or topical medications.`,
+        "img":"img_1713453615714.jpg" 
+      },
+      {
+        "name": "Eczema",
+        "symptoms": "Eczema typically presents as red, inflamed, and itchy patches of skin. These patches may be dry, scaly, or oozing. Common areas affected include the hands, feet, elbows, knees, and face.",
+        "remedies": "Treatment for eczema focuses on relieving symptoms and preventing flare-ups. This may include moisturizing the skin regularly, avoiding triggers such as irritants and allergens, using topical corticosteroids or immunomodulators to reduce inflammation, and practicing good skincare habits.",
+        "img":"Actinic"
+      },
+      {
+        "name": "Melanoma Skin Cancer Nevi and Moles",
+        "symptoms": "Melanoma may present as new or changing moles that exhibit asymmetry, irregular borders, multiple colors, and a large diameter. Nevi (moles) may be benign, but changes in size, shape, or color should be evaluated.",
+        "remedies": "Treatment for melanoma involves surgical removal of the cancerous tissue, sometimes followed by additional therapies like chemotherapy, immunotherapy, or targeted therapy. Regular skin checks and sun protection are also important for prevention.",
+        "img":"Actinic"
+      },
+      {
+        "name": "Psoriasis pictures Lichen Planus and related diseases",
+        "symptoms": "Psoriasis presents as red, scaly patches of skin often accompanied by itching or burning. Lichen planus may appear as shiny, flat-topped bumps or as patches of purplish, itchy skin.",
+        "remedies": "Treatment for psoriasis may include topical medications, phototherapy, systemic medications, or biologic drugs. Lichen planus treatment may involve topical or oral steroids, antihistamines, or immunosuppressants.",
+        "img":"Actinic"
+      },
+      {
+        "name": "Tinea Ringworm Candidiasis and other Fungal Infections",
+        "symptoms": "Fungal infections like ringworm (tinea) may cause red, scaly patches with raised borders and central clearing. Candidiasis may present as red, itchy rashes with satellite lesions or as white patches in moist areas.",
+        "remedies": "Treatment for fungal infections typically involves antifungal medications, either topical or oral, depending on the severity and location of the infection. Keeping the affected area clean and dry can also help prevent recurrence.",
+        "img":"Actinic"
+      },
+      {
+        "name": "Nail Fungus and other Nail Disease",
+        "symptoms": "Nail fungus (onychomycosis) may cause thickened, discolored, brittle nails with crumbling edges. Other nail diseases may include psoriasis of the nails, nail trauma, or bacterial infections.",
+        "remedies": "Treatment for nail fungus may involve topical or oral antifungal medications, as well as keeping nails trimmed and dry. Other nail diseases may require specific treatments depending on the underlying cause.",
+        "img":"Actinic"
+      },
+      {
+        "name": "Urticaria Hives",
+        "symptoms": "Urticaria (hives) appears as raised, itchy welts or bumps on the skin that may vary in size and shape. They can be triggered by allergic reactions, infections, medications, or stress.",
+        "remedies": "Treatment for hives may include antihistamines to relieve itching and inflammation, as well as identifying and avoiding triggers. Severe cases may require oral corticosteroids or other medications.",
+        "img":"Actinic"
+      }   
+]
+
+export default data;
